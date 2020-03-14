cb49eed243792816cee7e8b18da3a7c91e07f585
Christopher Baker <christopher@bitovi.com>
2020-03-13 20:10:42 -0700

    Updates

---
+++ precache-manifest.1e27e80a75095fc418a526c6b49a68bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c34eae20d45058a63fe7e788769c04d",
+    "url": "/react-pmo/index.html"
+  },
+  {
+    "revision": "5b6278fe52b84c16fae5",
+    "url": "/react-pmo/static/css/2.7ed83066.chunk.css"
+  },
+  {
+    "revision": "6eb1792e49134d263b47",
+    "url": "/react-pmo/static/css/main.9c7a94a1.chunk.css"
+  },
+  {
+    "revision": "5b6278fe52b84c16fae5",
+    "url": "/react-pmo/static/js/2.cd7b44dd.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/react-pmo/static/js/2.cd7b44dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eb1792e49134d263b47",
+    "url": "/react-pmo/static/js/main.ce114c82.chunk.js"
+  },
+  {
+    "revision": "7b37e2ce6caf4ca4a751",
+    "url": "/react-pmo/static/js/runtime-main.e6b16a9b.js"
+  },
+  {
+    "revision": "4a9d5f8f3dff38ebf47f6d11fb2d9582",
+    "url": "/react-pmo/static/media/homepage-hero.4a9d5f8f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3ff1c9e2eeaddf9390b1f6baa524c85f.js"
+  "/react-pmo/precache-manifest.1e27e80a75095fc418a526c6b49a68bd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-pmo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
